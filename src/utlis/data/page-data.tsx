0a6038b6882144668992afa9d43dd2a536833ce2
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-04-03 18:42:35 +0500

    Organized SMC data structure to connect contentful

--- src/utlis/data/page-data.tsx
+++ src/utlis/data/page-data.tsx
@@ -1,100 +1,131 @@
-import { Twitter, GitHub, FaceBook, LinkedIn } from "@/assets/icons/socialmedia"
+import {
+  Twitter,
+  GitHub,
+  FaceBook,
+  LinkedIn,
+} from "@/assets/icons/socialmedia";
 
 export const HeroData = [
-    {
-        call: `HELLO THERE 👋, I'M`,
-        head: 'Junaid',
-        details: 'Engineering your business stories and ideas creatively.'
-    },
-    {
-        call: 'C R E A T I V E  👇',
-        head: '<coder>',
-        details: `I am a creative Full stack developer who writes clean and efficient code.`
-    }
-]
+  {
+    call: `HELLO THERE 👋, I'M`,
+    head: "Junaid",
+    details: "Engineering your business stories and ideas creatively.",
+  },
+  {
+    call: "C R E A T I V E  👇",
+    head: "<coder>",
+    details: `I am a creative Full stack developer who writes clean and efficient code.`,
+  },
+];
 
 export const AboutMe = [
-    {
-        focusText: 'I am a Creative Developer who Engineer ideas and businesses Stories based in Lahore.',
-        details: 'Since October 2022 I am learning development with Sir Zia and PIAIC. When I am not doing development you will find me in AI space experimenting with different AI models or crafting business stories.',
-        Random: 'Random Facts',
-        RandomDetails: 'In 2021 I managed Infento (Netherlands) influencer marketing campaign. While studying charted accountancy I am slightly addicted to change. I may not be the smartest guy but a fast learner engineering ideas on the latest art of tech.'
-    },
-]
+  {
+    focusText:
+      "I am a Creative Developer who Engineer ideas and businesses Stories based in Lahore.",
+    details:
+      "Since October 2022 I am learning development with Sir Zia and PIAIC. When I am not doing development you will find me in AI space experimenting with different AI models or crafting business stories.",
+    Random: "Random Facts",
+    RandomDetails:
+      "In 2021 I managed Infento (Netherlands) influencer marketing campaign. While studying charted accountancy I am slightly addicted to change. I may not be the smartest guy but a fast learner engineering ideas on the latest art of tech.",
+  },
+];
 export const MyProjects = [
-    {
-        title: `TypeScript and Node.js Projects`,
-        brief: `Developed 11 different typescript and NodeJS projects including:`,
-        points: [`Student Management System`, `ATM & OOP Concepts`, `Calculator & To Do APP`, `Word Counter & Quiz`],
-        // remark: 'Try them on your CLI.',
-        gradient: ' bg-gradient-to-br from-blue-light to-grey-white',
-        GithubRepo: 'https://github.com/mjunaidca/TypeScript-Node-Projects'
-    },
-    {
-        title: `Panavere DAO Site & Styling Projects`,
-        brief: `Developed Panaverse DAO website design in Figma, & created frontend in:`,
-        points: [`Chakra UI + NextJS 13`, `Tailwind CSS + NextJS`],
-        remark: 'For practice created NextJS styling projects.',
-        gradient: ' bg-gradient-to-br from-purple-light to-pink-white',
-        GithubRepo: 'https://github.com/mjunaidca/PanaFig-1.0'
-    },
-    {
-        title: `AI Image Generator`,
-        brief: `Development of an AI Image Generator Runtime App with:`,
-        points: [`NextJS 13 framwork`, `Tailwind CSS`, `Headless UI`, `Open AI API`],
-        remark: 'Developed together with Muhammad Ahmad',
-        gradient: ' bg-gradient-to-br from-pink-light to-yellow-light',
-        GithubRepo: 'https://github.com/mjunaidca/image-ai'
-    }
-]
+  {
+    title: `TypeScript and Node.js Projects`,
+    brief: `Developed 11 different typescript and NodeJS projects including:`,
+    points: [
+      `Student Management System`,
+      `ATM & OOP Concepts`,
+      `Calculator & To Do APP`,
+      `Word Counter & Quiz`,
+    ],
+    // remark: 'Try them on your CLI.',
+    gradient: " bg-gradient-to-br from-blue-light to-grey-white",
+    GithubRepo: "https://github.com/mjunaidca/TypeScript-Node-Projects",
+  },
+  {
+    title: `Panavere DAO Site & Styling Projects`,
+    brief: `Developed Panaverse DAO website design in Figma, & created frontend in:`,
+    points: [`Chakra UI + NextJS 13`, `Tailwind CSS + NextJS`],
+    remark: "For practice created NextJS styling projects.",
+    gradient: " bg-gradient-to-br from-purple-light to-pink-white",
+    GithubRepo: "https://github.com/mjunaidca/PanaFig-1.0",
+  },
+  {
+    title: `AI Image Generator`,
+    brief: `Development of an AI Image Generator Runtime App with:`,
+    points: [
+      `NextJS 13 framwork`,
+      `Tailwind CSS`,
+      `Headless UI`,
+      `Open AI API`,
+    ],
+    remark: "Developed together with Muhammad Ahmad",
+    gradient: " bg-gradient-to-br from-pink-light to-yellow-light",
+    GithubRepo: "https://github.com/mjunaidca/image-ai",
+  },
+];
 
 export const MyAcheivments = [
-    {
-        title: `COURSERA`,
-        brief: `I had the pleasure to finish a few Google certifications on Coursera to enhance my knowledge and Skillset.`,
-        points: [`Introduction to Git and GitHub`, `Foundations of Project Management`, `Project Initiation: Starting a Successful Project`, `Foundations of User Experience (UX) Design`],
-        gradient: ' bg-gradient-to-br from-skin to-yellow-light '
-    },
-    {
-        title: `PIAIC QUARTER 1`,
-        brief: `I started programming with PIAIC in Oct 2022. By now I have learned:`,
-        points: [`HTML, CSS, Git & Github`, `Web 3.0 & Metaverse Theory`, 'Typescript Fundamentals', 'Object-Oriented TypeScript Programming', `Next.js 13 Web Dev`, `Chakra UI & Tailwind CSS`],
-        gradient: ' bg-gradient-to-br from-parrot to-grey-white'
-    },
-    {
-        title: `DATACAMP`,
-        brief: `During Quarter 1 break assignments with DataCamp I have completed the certification`,
-        points: [`Introduction to Git`],
-        gradient: ' bg-gradient-to-br from-royalblue to-grey-white'
-    }
-]
+  {
+    title: `COURSERA`,
+    brief: `I had the pleasure to finish a few Google certifications on Coursera to enhance my knowledge and Skillset.`,
+    points: [
+      `Introduction to Git and GitHub`,
+      `Foundations of Project Management`,
+      `Project Initiation: Starting a Successful Project`,
+      `Foundations of User Experience (UX) Design`,
+    ],
+    gradient: " bg-gradient-to-br from-skin to-yellow-light ",
+  },
+  {
+    title: `PIAIC QUARTER 1`,
+    brief: `I started programming with PIAIC in Oct 2022. By now I have learned:`,
+    points: [
+      `HTML, CSS, Git & Github`,
+      `Web 3.0 & Metaverse Theory`,
+      "Typescript Fundamentals",
+      "Object-Oriented TypeScript Programming",
+      `Next.js 13 Web Dev`,
+      `Chakra UI & Tailwind CSS`,
+    ],
+    gradient: " bg-gradient-to-br from-parrot to-grey-white",
+  },
+  {
+    title: `DATACAMP`,
+    brief: `During Quarter 1 break assignments with DataCamp I have completed the certification`,
+    points: [`Introduction to Git`],
+    gradient: " bg-gradient-to-br from-royalblue to-grey-white",
+  },
+];
 
 export const ContactME = {
-    name: `Muhammad Junaid Shaukat`,
-    email: `mr.junaidshaukat@gmail.com`,
-    number: `+92 320 9400 351`,
-    socialMedia: [
-        {
-            label: `twitter`,
-            Icon: `<Twitter />`,
-            link: `https://twitter.com/Muhamma76571391`
-        },
-        {
-            label: `github`,
-            Icon: `<GitHub />`,
-            link: `https://github.com/mjunaidca`
-        },
-        {
-            label: `facebook`,
-            Icon: `<FaceBook />`,
-            link: `https://www.facebook.com/mjunaid.ca/`
-        },
-        {
-            label: `linkedin`,
-            Icon: `<LinkedIn />`,
-            link: `https://www.linkedin.com/in/mrjunaid-ca1/`
-        }
-    ],
-    Quotes: [`Every businesses and idea have a great story to share creatively`]
-
-}
+  title: `CONTACT ME -`,
+  name: `Muhammad Junaid Shaukat`,
+  email: `mr.junaidshaukat@gmail.com`,
+  number: `+92 320 9400 351`,
+  socialMedia: [
+    {
+      label: "Twitter",
+      link: "https://twitter.com/Muhamma76571391",
+      Icon: Twitter,
+    },
+    {
+      label: "GitHub",
+      link: "https://github.com/mjunaidca",
+      Icon: GitHub,
+    },
+    {
+      label: "FaceBook",
+      link: "https://www.facebook.com/mjunaid.ca/",
+      Icon: FaceBook,
+    },
+    {
+      label: "LinkedIn",
+      link: "https://www.linkedin.com/in/mrjunaid-ca1/",
+      Icon: LinkedIn,
+    },
+  ],
+  Quotes: [`Every businesses and idea have a great story to share creatively`],
+  Copyright: " © 2023 Muhammad Junaid Shaukat",
+};
--- src/views/home/Contact.tsx
+++ src/views/home/Contact.tsx
@@ -1,71 +1,68 @@
-import Heading2 from '@/components/Heading2'
-import Wrapper from '@/components/Wrapper'
-import Image from 'next/image';
-import MapImage from '../../assets/images/map.png'
-import { ContactME } from '@/utlis/data/page-data';
-import MyVector from '../../assets/images/Vector.png'
-import Quote from '../../assets/images/Quote.png'
-import { Twitter, GitHub, FaceBook, LinkedIn } from "@/assets/icons/socialmedia"
-import Link from 'next/link';
-
-
+import Heading2 from "@/components/Heading2";
+import Wrapper from "@/components/Wrapper";
+import Image from "next/image";
+import MapImage from "../../assets/images/map.png";
+import { ContactME } from "@/utlis/data/page-data";
+import MyVector from "../../assets/images/Vector.png";
+import Quote from "../../assets/images/Quote.png";
+import {
+  Twitter,
+  GitHub,
+  FaceBook,
+  LinkedIn,
+} from "@/assets/icons/socialmedia";
+import Link from "next/link";
+import IconText from "@/components/IconText";
 
 const Contact = () => {
-    return (
-        <div>
-            <Wrapper>
-                <Heading2 text={`LET'S TALK -`} />
-
-                <div className='flex justify-center px-4 sm:px-0 '>
-                    <Image src={MapImage} alt="Maps" />
-                    <div className="absolute base:w-auto max-w-sm sm:max-w-lg md:max-w-2xl lg:max-w-3xl xl:max-w-4xl md:w-full mx-8 md:mx-14 mt-10 max-h-112 md:h-full md:max-h-56 lg:max-h-72 bg-gradient-to-br from-lime to-apricoat shadow-md">
-                        <div className='flex flex-col md:flex-row md:h-full'>
-                            <div className='sm:basis-4/12 basis-1/6  xl:px-12 sm:px-6  py-3 flex items-center justify-center '>
-                                <Image src={MyVector} alt={'MyVector'} />
-                            </div>
-                            <div className='sm:basis-4/12 basis-5/6 flex flex-col py-3 items-start justify-center gap-3 px-6'>
-                                <div className='font-semibold text-lg md:text-xl lg:text-2xl text-grey'>
-                                    {ContactME.name}
-                                </div>
-                                <div className='font-semibold text-sm md:text-lg lg:text-xl text-grey-dark'>
-                                    {ContactME.email}
-                                </div>
-                                <div className='font-normal lg:text-lg text-grey-light pt-3'>
-                                    {ContactME.number}
-                                </div>
-                                <div className='flex flex-row gap-5 py-2 self-center'>
-                                    <Link href={'https://twitter.com/Muhamma76571391'}>
-                                        <Twitter />
-                                    </Link>
-                                    <Link href={'https://github.com/mjunaidca'}>
-                                        <GitHub />
-                                    </Link>
-                                    <Link href={'https://www.facebook.com/mjunaid.ca/'}>
-                                        <FaceBook />
-                                    </Link>
-                                    <Link href={'https://www.linkedin.com/in/mrjunaid-ca1/'}>
-                                        <LinkedIn />
-                                    </Link>
-                                </div>
-
-                            </div>
-                            <div className='hidden md:block basis-4/12 flex-col items-baseline my-auto justify-between gap-2'>
-                                <div className='w-6 lg:w-8'>
-                                    <Image src={Quote} alt={'Quote'} />
-                                </div>
-                                <div className='px-3 font-semibold text-md lg:text-lg xl:text-xl text-grey-dark'>
-                                    {ContactME.Quotes}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <div>
+      <Wrapper>
+        <Heading2 text={ContactME.title} />
+        <div className="flex justify-center px-4 sm:px-0 ">
+          <Image src={MapImage} alt="Maps" />
+          <div className="absolute base:w-auto max-w-sm sm:max-w-lg md:max-w-2xl lg:max-w-3xl xl:max-w-4xl md:w-full mx-8 md:mx-14 mt-10 max-h-112 md:h-full md:max-h-56 lg:max-h-72 bg-gradient-to-br from-lime to-apricoat shadow-md">
+            <div className="flex flex-col md:flex-row md:h-full">
+              <div className="sm:basis-4/12 basis-1/6  xl:px-12 sm:px-6  py-3 flex items-center justify-center ">
+                <Image src={MyVector} alt={"MyVector"} />
+              </div>
+              <div className="sm:basis-4/12 basis-5/6 flex flex-col py-3 items-start justify-center gap-3 px-6">
+                <div className="font-semibold text-lg md:text-xl lg:text-2xl text-grey">
+                  {ContactME.name}
                 </div>
-                <div className='font-semibold text-base lg:text-xl text-grey-dark pt-96 mt-24 sm:mt-0 md:pt-16 lg:pt-18 pb-5 md:pb-5 lg:pb-7 '>
-                    CC 2023 Muhammad Junaid Shaukat
+                <div className="font-semibold text-sm md:text-lg lg:text-xl text-grey-dark">
+                  {ContactME.email}
                 </div>
-            </Wrapper>
+                <div className="font-normal lg:text-lg text-grey-light pt-3">
+                  {ContactME.number}
+                </div>
+                <div className="flex flex-row gap-5 py-2 self-center">
+                  {ContactME.socialMedia.map((item: any) => (
+                    <IconText
+                      key={item.label}
+                      Icon={item.Icon}
+                      link={item.link}
+                    />
+                  ))}
+                </div>
+              </div>
+              <div className="hidden md:block basis-4/12 flex-col items-baseline my-auto justify-between gap-2">
+                <div className="w-6 lg:w-8">
+                  <Image src={Quote} alt={"Quote"} />
+                </div>
+                <div className="px-3 font-semibold text-md lg:text-lg xl:text-xl text-grey-dark">
+                  {ContactME.Quotes}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+        <div className="font-semibold text-base lg:text-xl text-grey-dark pt-96 mt-24 sm:mt-0 md:pt-16 lg:pt-18 pb-5 md:pb-5 lg:pb-7 ">
+          {ContactME.Copyright}
+        </div>
+      </Wrapper>
+    </div>
+  );
+};
 
 export default Contact;
